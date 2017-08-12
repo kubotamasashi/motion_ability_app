369bc8966c8e51087ae309e6b761f5e4d4dec235
kubota masashi <jyvina@gmail.com>
2017-08-12 08:00:09 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MotionAbilityApp::Application.config.secret_key_base = '80eff667e5c21f79adb4717351fe234ff65361b5dd619ab05ec3861c1baa682bc0e7e2f55c28d41ece6624b0b83ff96ceaf1e7163e5433d3a6d7142248cac6bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MotionAbilityApp::Application.config.session_store :cookie_store, key: '_motion_ability_app_session'
